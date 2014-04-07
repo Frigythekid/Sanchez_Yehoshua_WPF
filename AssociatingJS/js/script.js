9e68f5e11707030e87dd443893fae24ef1d3fdec
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-07 16:33:19 -0400

    creating an Array while following the screencast.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,7 +1,2 @@
-// Variables of Different Types, Number, Booleans and Strings
-
-var age = 24; // Number variable
-
-var isStudent = false; //Boolean Variable
-
-var firstName = "Josh";
+// Arrays
+var muppetNames = ["kermit" , "Miss Piggy", "Gonzo", "Rizzo"];
