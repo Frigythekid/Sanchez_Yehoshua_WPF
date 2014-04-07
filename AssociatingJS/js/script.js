4b37c898ccc1a8492a42040cff8ef43949a0466d
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-07 16:39:50 -0400

    changed variables from 1 and 2 to change the name.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,2 +1,9 @@
 // Arrays
-var muppetNames = ["kermit" , "Miss Piggy", "Gonzo", "Rizzo"];
+var muppetNames = ["kermit" , "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+muppetNames[1] = "Rizzo"; //changes index #1 to Rizzo
+muppetNames[2] = "Fozzy";
+
+var num = 2;
+
+console.log(muppetNames[num]); //lets me see whats in the index #1
