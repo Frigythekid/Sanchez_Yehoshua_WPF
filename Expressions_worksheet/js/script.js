0dd4bbd11075b00d80d71c44ac4c4c422fea282d
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-08 00:44:18 -0400

    Completed script for problem 1.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,3 +1,9 @@
 //Yehoshua Sanchez April 7, 2014 Expressions Worksheet
 //Calculate Sparky's age is in dog years
 var sparkysAge = 2;
+
+//multiply sparkysAge by 7 and apply to dogYears
+var dogYears = sparkysAge * 7
+
+//print out sparkysAge in dog years
+console.log("Sparky is " + sparkysAge + " human years old which is " + dogYears + " in dog years.")
