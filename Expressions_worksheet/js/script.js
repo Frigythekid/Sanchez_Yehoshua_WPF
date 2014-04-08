69de3d9adf153e2963284f9571e2b85b96218a4d
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-08 00:57:19 -0400

    finished script for problem 2. Still need to check calculations.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,9 +1,23 @@
 //Yehoshua Sanchez April 7, 2014 Expressions Worksheet
+//Dog years problem
 //Calculate Sparky's age is in dog years
 var sparkysAge = 2;
 
 //multiply sparkysAge by 7 and apply to dogYears
-var dogYears = sparkysAge * 7
+var dogYears = sparkysAge * 7;
 
 //print out sparkysAge in dog years
 console.log("Sparky is " + sparkysAge + " human years old which is " + dogYears + " in dog years.")
+
+
+//Slice of Pie part 1
+//Calculate how many slices of pizza each person will get
+var slices = 10;
+var people = 8;
+var pizzas = 10;
+
+//Multiply pizzas by slices and divide by people and apply to slices eaten
+var slicesEaten = pizzas * slices / people;
+
+//print out the number of slices eaten by the people
+console.log("Each person ate " + slicesEaten + " slices of pizza at the party." )
