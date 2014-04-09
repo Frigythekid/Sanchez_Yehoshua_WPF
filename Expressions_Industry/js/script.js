911d73269f49b5b75ec2a4836d8de0cf55cf27c6
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-09 16:55:18 -0400

    Changed array to a single prompt.
    Array didn’t work. changed to a single variable.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,9 +1,13 @@
 //Yehoshua "Josh" Sanchez
 //04-9-14 Expressions_Industry
-//script to figure out the final price of a job
+//script to let a locksmith figure out the final price of a job
 //need to take the amount of doors per floor and floors per building, also the price per lock
 
 alert("First we need to find out how many locks are in the bulding.");
 //alert the user of what will be needed
 
-var locksPerFloor = [prompt("How many locks are on the 1st Floor"), prompt(On the 2nd?), prompt(On the 3rd?), prompt(On the 4th?), prompt(On the 5th?)];
+var floors = prompt("How many floors are in the building")
+//Asks the user the number of locks per floor
+
+console.log(floors);
+
