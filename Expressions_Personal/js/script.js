4f7212186c1003c864781dcae46cf87e87ef368d
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-09 15:05:12 -0400

    Set up initial prompts.
    Set up the initial prompts to get the needed variables.

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,7 @@
+//Yehoshua "Josh" Sanchez
+//04-9-14 Expressions_Personal
+
+alert("I need to ask you a few questions."); //nice alert letting user know some questions will be asked
+var carGallons = prompt("How many gallons does your vehicle hold?"); //Ask user to enter the amount of gallons in their vehicle
+var milesPer = prompt("How many miles per gallon does your vehicle get?"); //Ask user to enter the amount of miles per gallon their vehicle gets
+var tripMiles = prompt("How many miles is your trip?"); //Ask user how many miles their trip is
