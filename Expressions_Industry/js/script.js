fb27f136008cf25f3186a14b551950b9456ea551
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-09 16:47:35 -0400

    Added initial variable as an array.
    Asks the user the number of locks per floor.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,3 +2,8 @@
 //04-9-14 Expressions_Industry
 //script to figure out the final price of a job
 //need to take the amount of doors per floor and floors per building, also the price per lock
+
+alert("First we need to find out how many locks are in the bulding.");
+//alert the user of what will be needed
+
+var locksPerFloor = [prompt("How many locks are on the 1st Floor"), prompt(On the 2nd?), prompt(On the 3rd?), prompt(On the 4th?), prompt(On the 5th?)];
