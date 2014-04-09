512e91eb7f157753abf2e164a9783789b08028cc
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-09 15:42:00 -0400

    Changed result to be displayed in a sentence.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,4 +7,4 @@ var milesPer = prompt("How many miles per gallon does your vehicle get?"); //Ask
 var tripMiles = prompt("How many miles is your trip?"); //Ask user how many miles their trip is
 //need expression to calculate the amount of stops needed for the trip
 var stops = tripMiles / (carGallons * milesPer); // Multiply carGallons with milesPer and then divide tripMiles by the result of the initial multiplication
-console.log(parseInt(stops)); //prints the result of the equation
+console.log("You would need to stop " + parseInt(stops) + " times when traveling " + " miles."); //prints the result of the equation
