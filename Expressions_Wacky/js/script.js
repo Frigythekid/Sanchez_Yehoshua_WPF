435b09a29d5e515fad8a2ec83bdabbe80ed71a09
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-09 20:32:14 -0400

    Changed code
    Changed code to use arrays in order to calculate the answers.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,16 +1,23 @@
 //Yehoshua "Josh" Sanchez
 //04-9-14 Expressions_Wacky
 //Code to detect the superhero member
-alert("Hello, I am about to ask you some questions."); //alert to let the user know some questions will be asked
-var color = prompt("What is your favorite color between Red, Blue, or Green?"); //prompt to get the first variable
+alert("Hello, I am about to ask you to pick some numbers."); //alert to let the user know some questions will be asked
+var firstNum = prompt("Pick a number between 0 and 11"); //prompt to get the first variable
 
-var number = prompt("Pick a number between 1 and 10"); //prompt to get the second variable
+var secondNum = prompt("Pick a number between 0 and 6"); //prompt to get the second variable
 
-var monthBorn = prompt("What month were you born?"); //prompt to get the third variable
+var thirdNum = prompt("Pick a number between 0 and 5"); //prompt to get the third variable
 
-var phoneChoice = prompt("Which phone do you prefer \n iPhone, Android, or Windows?"); //prompt to get the last variable
+var fourthNum = prompt("Lastly, pick a number between 40 and 70"); //prompt to get the last variable
 
-console.log(color);
-console.log(number);
-console.log(monthBorn);
-console.log(phoneChoice);
+
+//added console.log for each variable to make sure the information was recorded
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//establishes the first variable as a month in the year
+
+var superHero = ["Batgirl", "Superman", "Flash", "Wonder Woman", "Green Lantern", "Batman", "Supergirl"]; //changes the second variable into a superhero
+
+var villain = ["Harley Quinn", "The Joker", "Lex Luthor", "Sinestro", "Cheetah", "Death Stroke"];
+console.log(month[firstNum]);
+console.log(superHero[secondNum]);
+console.log(villain[thirdNum]);
