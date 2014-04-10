55a41b948418844a1eaad873a664e124e2c211b5
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-09 20:39:11 -0400

    All variables added.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,16 +8,29 @@ var secondNum = prompt("Pick a number between 0 and 6"); //prompt to get the sec
 
 var thirdNum = prompt("Pick a number between 0 and 5"); //prompt to get the third variable
 
-var fourthNum = prompt("Lastly, pick a number between 40 and 70"); //prompt to get the last variable
+var fourthNum = prompt("Lastly, pick a number between 40 and 70"); //prompt to get the fourth variable
 
-
-//added console.log for each variable to make sure the information was recorded
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 //establishes the first variable as a month in the year
 
 var superHero = ["Batgirl", "Superman", "Flash", "Wonder Woman", "Green Lantern", "Batman", "Supergirl"]; //changes the second variable into a superhero
 
-var villain = ["Harley Quinn", "The Joker", "Lex Luthor", "Sinestro", "Cheetah", "Death Stroke"];
+var villain = ["Harley Quinn", "The Joker", "Lex Luthor", "Sinestro", "Cheetah", "Death Stroke"]; //changes the third variable into the villain
+
+var age = fourthNum / 2 //divides the fourth variable by 2 to get the age
+
+
 console.log(month[firstNum]);
 console.log(superHero[secondNum]);
 console.log(villain[thirdNum]);
+//added console.log for each variable to make sure the information was recorded
+
+alert
+
+
+
+
+
+
+
+
