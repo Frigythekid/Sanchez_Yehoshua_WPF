09dc0dae643451d3efdf1fd6eb9a36fdae775a17
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-14 15:09:50 -0400

    Following along with screencast.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,8 @@
 //conditional logic
 
-var oldEnough = true;
-
-if(oldEnough){
+var kidHeight = 50;
+var minHeight = 48;
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
 	console.log("You can ride the coaster");
 }
-console.log("what comes after");
