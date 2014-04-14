181d2d8bd41233704273efd7257239178192c6ea
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-14 15:21:14 -0400

    else if screencast.

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -2,11 +2,16 @@
 
 var kidHeight = 47;
 var minHeight = 48;
+var wParentHeight = 45;
 
 //if the kid is over 48 inches in height
 if(kidHeight >= minHeight){
 	console.log("You can ride the coaster");
 }
+else if(kidHeight > wParentHeight){
+	console.log("You can ride, but only with a parent present.");
+}
 else{
-	console.log("Sorry kid, you've got some growing to do first.");
+		console.log("Sorry kid, you've got some growing to do first.");
+
 }
