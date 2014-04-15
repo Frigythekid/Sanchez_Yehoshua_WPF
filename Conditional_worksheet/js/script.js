72e17c667b9ccd1d05da30e5ff6450eba51b5076
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-14 20:39:18 -0400

    Tested the code with different variables.

--- Conditional_worksheet/js/script.js
+++ Conditional_worksheet/js/script.js
@@ -3,13 +3,13 @@
 //Last Chance for Gas!
 //Is there enough gas left to make it to the next gas station 200 miles away
 //variables needed to complete the expression
-var mpg = //variable for the amount of miles the car gets per gallon
-var gasLeftPercent = //percent left in the gas tank
-var gasTank = //Total number of gallons the vehicle holds
+var mpg = 15;//variable for the amount of miles the car gets per gallon
+var gasLeftPercent = 40//percent left in the gas tank
+var gasTank = 20;//Total number of gallons the vehicle holds
 var miles = 200; //constant in the equation
 //variable needed to calculate the gas left in gallons
 var gallonsLeft = gasLeftPercent / 100 * gasTank;
-if(gallonsLeft > miles){
+if(gallonsLeft * mpg > miles){
 	//able to make it to the next station
 	console.log("Yes, you can make it without stopping for gas!");
 }else{
