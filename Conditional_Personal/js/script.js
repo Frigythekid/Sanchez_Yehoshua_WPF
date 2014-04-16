ec27258efe6405fd456bb70e35a7102080a3d67a
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-16 13:50:51 -0400

    Intial test of empty string.

---
+++ Conditional_Personal/js/script.js
@@ -0,0 +1,9 @@
+//Yehoshua "Josh" Sanchez
+//Conditionals Personal
+//script to find if I have done enough Excercise
+alert("Hello, I am going to ask you some questions.");//introductory alert
+var runMinutes = prompt("How many minutes did you run?");
+if(runMinutes = ""){
+	var runMinutes = prompt("You forgot to input something!");
+}
+console.log(runMinutes);
