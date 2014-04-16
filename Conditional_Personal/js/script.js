3069283cae3d4746c74dfc6c59cbcefe72933e77
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-16 15:19:50 -0400

    Further testing.

--- Conditional_Personal/js/script.js
+++ Conditional_Personal/js/script.js
@@ -6,29 +6,43 @@ var runMinutes = prompt("How many minutes did you run?");
 if(runMinutes === ""){
 	var runMinutes = prompt("You forgot to input something!");
 }
-if(runMinutes > 75){
+else if(runMinutes > 75){
 	alert("That was a nice run!");
-}else{
-	alert("Great! Next question.");
 }
+
+alert("Great! Next question.");
+
 
 var pushUps = prompt("How many push ups did you do?");
 if(pushUps === ""){
 	var pushUps = prompt("You forgot to input something!");
 }else if(pushUps > 50){
 	alert("Well aren't you strong.");
-}else{
-	alert("Thank you! Just one last question.")
 }
+
+alert("Thank you! Just one last question.")
+
 
 var sitUps = prompt("How many sit ups did you do?");
 if(sitUps === ""){
 	var sitUps = prompt("You forgot to input something!");
+}else if(sitUps > 75){
+	alert("Great work!")
+}
+
+var totalUps = sitUps + pushUps;
+
+if(runMinutes >= 45 && totalUps >= 75){
+	alert("You have done plenty of excercise, now go get some rest!");
+}else if(runMinutes >= 90 || totalUps >= 125){
+	alert("You have done plenty of excercise, now get some rest!");
+}else{
+	alert("Hmm...Maybe you should try working out a bit more.");
 }
 console.log(runMinutes);
 console.log(pushUps);
 console.log(sitUps);
-
+console.log(totalUps);
 
 
 
