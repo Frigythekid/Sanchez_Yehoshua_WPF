69ff0bb9ef21b73e5d5cdecde95473295325e82c
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-17 18:23:20 -0400

    Added semi-colons.
    Also added part of the final conditionals.

--- Conditional_Wacky/js/script.js
+++ Conditional_Wacky/js/script.js
@@ -8,9 +8,9 @@ alert("With just 3 questions I will be able \n to tell you how many wins you wil
 var height = prompt("First things first. \n What is your height in inches.");//prompt to get the variable for height
 
 if(height <= 65){
-	alert("You probably won't stand a chance but let's keep going.")
+	alert("You probably won't stand a chance but let's keep going.");
 	//if the user is shorter than 65 inches an alert and console notification is displayed stating that the user is a little short
-	console.log("You probably won't stand a chance but let's keep going.")
+	console.log("You probably won't stand a chance but let's keep going.");
 }else if(height >= 89){
 	alert("You should probably be playing basketball!");
 	//if the user is taller than 89 inches an alert and console notification is displayed stating that the user should be playing basketball
@@ -26,18 +26,35 @@ if(age <= 17){
 	alert("You are very young but we can make it work!");
 	//if the age entered is less then 17 than an alert and console notification displays saying the user is very young
 	console.log("You are very young but we can make it work!");
-}else if(age >= 50){
+}else if(age >= 40){
 	alert("You should probably be retired, or worse dead!");
 	//if the age entered is greater than 50 then an alert and console notification display stating the user is very old.
 	console.log("You should probably be retired, or worse dead!");
 }else{
-	alert("Alright, let's continue.")
+	alert("Alright, now to figure out how many wins you'll get.")
 	//if the age does not match any other conditional then an alert and notification appear stating to continue with the question.
-	console.log("Alright, let's continue.")
+	console.log("Alright, now to figure out how many wins you'll get.")
 }
 
+if(age > 17 && age < 40){
+	win1 = 10;
+}else if(age <= 17){
+	win1 = 6;
+}else{
+	win1 = 3;
+}
 
+if(height > 65 && height < 89){
+	win2 = 10;
+}else if(height <= 65){
+	win2 = 4;
+}else{
+	win2 = 5;
+}
 
+var totalWins = win1 + win2
+ 
+ if(totalWins)
 
 
 
