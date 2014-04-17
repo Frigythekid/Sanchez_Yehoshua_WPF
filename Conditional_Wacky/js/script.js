e6cce26b5f817ba02d17985982552a7c8953fb15
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-16 22:18:59 -0400

    First variable and initial alerts entered.

---
+++ Conditional_Wacky/js/script.js
@@ -0,0 +1,16 @@
+//Yehoshua "Josh" Sanchez
+//Conditionals Wacky
+//script to figure out how many wins you would get as a pitcher
+
+alert("You are a professional pitcher for the Atlanta Braves.")//alert to let the user know what is happening
+alert("With just 3 questions I will be able \n to tell you how many wins you will get in one year.");//alert to tell the user what will be figured out
+
+var height = prompt("First things first. \n What is your height in inches.");//prompt to get the variable for height
+
+if(height <= 65){
+	alert("You probably won't stand a chance but let's keep going.")
+	console.log("You probably won't stand a chance but let's keep going.")
+}else if(height >= 89){
+	alert("You should probably be playing basketball!");
+	console.log("You should probably be playing basketball!");
+}
