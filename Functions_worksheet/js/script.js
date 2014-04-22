a585fb168e8a1ca497199a0920a6cb30504f809b
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-22 12:11:16 -0400

    Tested code with another variable

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,7 +2,7 @@
 //April 22, 2014
 //Functions worksheet
 //Find the circumference of a circle
-var radius = 6;//given variable is the radius
+var radius = 5.6;//given variable is the radius
 var circumference = calcCirc(radius);//circumference variable with the fuction and argument for variable
 console.log("The circumference of the circle is " + circumference + ".");//prints to the console what the circumference is
 function calcCirc(r)//function with parameter for the radius
