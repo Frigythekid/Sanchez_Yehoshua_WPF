163d5bb4b78184ffcf7880261a5c8b116725d42b
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-22 12:02:55 -0400

    changed the console.log
    “The circumference of the circle is x.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -4,9 +4,9 @@
 //Find the circumference of a circle
 var radius = 4;//given variable is the radius
 var circumference = calcCirc(radius);
-console.log(circumference);
+console.log("The circumference of the circle is " + circumference + ".");
 function calcCirc(r)
 {
-	return r * 2 * 3.14;
+	return 2 * 3.14 * r;
 }
 
