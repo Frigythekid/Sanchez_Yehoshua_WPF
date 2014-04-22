067ca34b94b1aa8db05d5445822e9e6e7062cfce
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-22 12:08:13 -0400

    Added comment to the function

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -5,7 +5,7 @@
 var radius = 4;//given variable is the radius
 var circumference = calcCirc(radius);//circumference variable with the fuction and argument for variable
 console.log("The circumference of the circle is " + circumference + ".");//prints to the console what the circumference is
-function calcCirc(r)
+function calcCirc(r)//function with parameter for the radius
 {
 	return 2 * 3.14 * r;
 }
