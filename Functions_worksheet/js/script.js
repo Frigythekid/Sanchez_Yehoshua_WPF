b0794bd22702d9d7994db250103fd0e5617ff148
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-22 12:41:41 -0400

    Tested both codes with different variables

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,7 +2,7 @@
 //April 22, 2014
 //Functions worksheet
 //Find the circumference of a circle
-var radius = 5.6;//given variable is the radius
+var radius = 10;//given variable is the radius
 var circumference = calcCirc(radius);//circumference variable with the fuction and argument for variable
 console.log("The circumference of the circle is " + circumference + ".");//prints to the console what the circumference is
 function calcCirc(r)//function with parameter for the radius
@@ -15,7 +15,7 @@ function calcCirc(r)//function with parameter for the radius
 
 //Stung
 //Calculate the number of bee sting are needed to kill an animal
-var weight = 250;//Given variable is the weight in pounds
+var weight = 1000;//Given variable is the weight in pounds
 var beeStings = 8.666666667;//variable for the bee stings needed per pound
 var beeStingTotal = calcBeeSting(weight, beeStings);//variable for total bee stings set to the function that will be used with the arguments for weight and beestings
 console.log("It takes " + beeStingTotal + " bee stings to kill this animal.");//Prints to the console how many bee stings it would take to kill the animal.
