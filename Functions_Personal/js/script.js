ef9f25c2a6c4bdae49c30cdf733aee761208e76d
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-28 09:45:10 -0400

    function stated for first sub assignment.

---
+++ Functions_Personal/js/script.js
@@ -0,0 +1,9 @@
+//Yehoshua "Josh" Sanchez
+//May 1, 2014
+//Functions Personal
+//Script to find how many hours there is left in the day
+var hoursLeft = function(s,v, w){
+	var hours = 24 - (s + v + w);
+	return hours
+}
+
