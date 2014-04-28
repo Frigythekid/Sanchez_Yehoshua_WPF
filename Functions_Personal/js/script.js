e7d57558a49c71029149e7d97d68209cc4605d51
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-28 10:28:48 -0400

    Main part of code written.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -2,8 +2,8 @@
 //May 1, 2014
 //Functions Personal
 //Script to find how many hours there is left in the day
-var hoursLeft = function(s,v, w){
-	var hours = 24 - (s + v + w);
+var hoursLeft = function(s, v, w, sw){
+	var hours = 24 - (s + v + w + sw);
 	return hours
 }
 
@@ -13,4 +13,20 @@ var games = prompt("Have you played any video games?")
 
 if(games === "yes"){
 	var videoGames = prompt("How many hours have you played?");
+}else{
+	var videoGames = 0;
 }
+
+var work = prompt("Have you worked today?");
+
+if(work === "yes"){
+	var workHours = prompt("How many hours have you worked?");
+}else{
+	var workHours = 0;
+}
+var schoolHours = prompt("How many hours have you spent on school?")
+
+var timeLeft = hoursLeft(Number(sleep), Number(videoGames), Number(workHours), Number(schoolHours));
+
+console.log(timeLeft);
+
