55fee732de73ae1f9eff8c614ea208d48b21dbeb
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-29 12:36:01 -0400

    changed prompt to confident variable.
    removed console.log

--- Functions_Industrial/js/script.js
+++ Functions_Industrial/js/script.js
@@ -6,9 +6,8 @@
 var studyHours = prompt("How many hours a day have you been studying?");
 var studyDays = prompt("How many days have you been studying?");
 var studyTotal = studyTime(studyHours, studyDays);
-var confident = prompt("How confident are you in your ability?");
+var confident = prompt("Are you confident that you've learned what you have studied?");
 (studyTotal >= 60 && confident === "yes") ? console.log("You can keep programming!") : console.log("You should probably go study some more.");
-console.log(studyTotal);
 function studyTime(h, d){
 	return h * d;
 }
