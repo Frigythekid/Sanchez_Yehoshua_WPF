7c332f02e04552c4c30375b1a73438d14b926b59
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-29 13:50:02 -0400

    prompt added for the first variable

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,6 @@
 //Yehoshua "Josh" Sanchez
 //May 1, 2014
 //Functions Wacky
+//find out if able to outrun a velociraptor
+alert("You are now being chased by a velociraptor.");
+var speed = prompt("How fast can you run in ft/s?");
