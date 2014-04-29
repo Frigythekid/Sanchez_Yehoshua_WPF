313084e2d56012d535199ff6c758e90a5b55022c
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-29 12:33:54 -0400

    code completed

--- Functions_Industrial/js/script.js
+++ Functions_Industrial/js/script.js
@@ -3,11 +3,12 @@
 //Functions Industrial
 //simple calculation to find if the user is ready to keep programming
 
-var studyHours = prompt("How many hours a day have you been studying?")
-var studyDays = prompt("How many days have you been studying?")
+var studyHours = prompt("How many hours a day have you been studying?");
+var studyDays = prompt("How many days have you been studying?");
 var studyTotal = studyTime(studyHours, studyDays);
+var confident = prompt("How confident are you in your ability?");
+(studyTotal >= 60 && confident === "yes") ? console.log("You can keep programming!") : console.log("You should probably go study some more.");
 console.log(studyTotal);
-
 function studyTime(h, d){
 	return h * d;
 }
