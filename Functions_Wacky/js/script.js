f01ee483f8f8b321b6d42f021cf564599b74cf81
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-29 14:43:30 -0400

    comments added

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,6 +3,7 @@
 //Functions Wacky
 //find out if able to outrun a velociraptor
 alert("You are now being chased by a velociraptor.");
+//alert to let the user know the situation
 var speed = prompt("How fast can you run in ft/s?");
 function velociraptorDistance(s){
 	return 1760 - (s * 30)
