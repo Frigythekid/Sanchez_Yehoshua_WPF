86d4001f643fbdec8b3d255ffbf752a8d07db7ac
Frigythekid <Sanchez.yehoshua@gmail.com>
2014-04-29 12:40:31 -0400

    comments added

--- Functions_Industrial/js/script.js
+++ Functions_Industrial/js/script.js
@@ -8,6 +8,7 @@ var studyHours = prompt("How many hours a day have you been studying?");
 var studyDays = prompt("How many days have you been studying?");
 //prompt to collect how many days the user has studied
 var studyTotal = studyTime(studyHours, studyDays);
+//variable calls the function to evaluate the total hours studied using parameters
 var confident = prompt("Are you confident that you've learned what you have studied?");
 (studyTotal >= 60 && confident === "yes") ? console.log("You can keep programming!") : console.log("You should probably go study some more.");
 function studyTime(h, d){
